3417378784fa1d2ecec648493ab894c823abad5f
Hector <hestrada498111@gmail.com>
2023-02-21 17:55:55 +0100

    [feat] hacemos import
    [feat] añadimos una condición para ejecutar el juego
    
    [ref] eliminamos algunos comentarios
    
    [fix] eliminamos la ejecución automatica del archivo g03_ppt.py
    
    [doc] modificamos el diccionario y añadimos nuestro juego

--- main_jocs.py
+++ main_jocs.py
@@ -19,6 +19,7 @@ from collections import defaultdict
 from termcolor import colored
 import f00_functions as f00
 import f01_data_base as fdb
+from g03_ppt import startPPT #Importamos el juego
 # Versió 1.0 cal importar el teu fitxer de jocs
 
 # Aquesta funció, demana a l'usuari un sencer per triar jocs
@@ -59,11 +60,8 @@ def playGame(whatGame):
     if whatGame == 0:
         # en veritat això no es un error, sino el codi d'error per sortir
         errorsInExecution = 1
-    # A la versió 1.0 hauréu de modificar aquest codi afegint alguna cosa
-    # similar al que poso a baix
-    # elif whatGame == X:
-    #    return(s) de la funció = com he anomenat el paquet del joc  .   funció per executar el joc seleccionat ()
-    #    errorsInExecution      =                                 g0X.startX 
+    elif (whatGame == 3): #Si el userInput == 3:
+        startPPT() # Ejecuto el Juego
     else:
         # Hi ha un error no identificat.
         errorsInExecution = 2
@@ -79,7 +77,8 @@ def main():
 
     # creo un diccionari amb els jocs instal·lats
     dictGames={
-        0: "Vull deixar de jugar"
+        0: "Vull deixar de jugar",
+        3: "Juego 3"
     }
     # A la versió 1.0 has d'afegir aquó el nom del teu joc.
     # Creo una llista de missatges per mostrar a la funció
